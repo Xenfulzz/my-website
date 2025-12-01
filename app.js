76e3f9817b7875e8265eb17174a74b7451504c61
Xenfulzz <contentwells@gmail.com>
2025-12-01 19:29:17 +0000

    Delete app.js

--- app.js
+++
@@ -1,4 +0,0 @@
-// Example: Open PayPal or donation link
-document.getElementById('donateBtn').addEventListener('click', function () {
-    window.open('https://paypal.me/Xenfulz', '_blank', 'noopener');
-});
