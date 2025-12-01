d4f3d7cd62dc05f9534b6f58ac2ee646bd5e707b
Xenfulzz <contentwells@gmail.com>
2025-12-01 17:11:49 +0000

    Implement donation button functionality
    Added a donation button that opens a PayPal link.

---
+++ app.js
@@ -0,0 +1,4 @@
+// Example: Open PayPal or donation link
+document.getElementById('donateBtn').addEventListener('click', function () {
+    window.open('https://paypal.me/Xenfulz', '_blank', 'noopener');
+});
